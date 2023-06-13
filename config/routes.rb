042f934afaf358a4cd5c992819d36647a02ce341
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-06-13 17:36:46 -0300

    feat: user name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   root 'pages#home'
   get 'pages/about'
   resources :articles
-
 end
